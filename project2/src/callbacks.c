64c6755717bcb53c8b8a13ac10a667589d7d6318
Mouhib001 <mouhib.bouazizi@esprit.tn>
2022-11-23 15:57:02 +0100

    after build

---
+++ project2/src/callbacks.c
@@ -0,0 +1,42 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_yz_AR_clicked                       (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_yz_supp_clicked                     (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_yz_modif_clicked                    (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_yz_Prec_clicked                     (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ project2/src/callbacks.h
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+
+
+void
+on_yz_AR_clicked                       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_yz_supp_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_yz_modif_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_yz_Prec_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
